91b57d516cae9348893451967c6a52f4f795693f
thermojam <nmensky@gmail.com>
2025-12-17 10:58:37 +0300

    using custom entities

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react-swc'
+import {defineConfig} from "vite"
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:3001",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+
+        open: true,
+        port: 3000,
+    },
 })
