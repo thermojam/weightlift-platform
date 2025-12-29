16e6feabb934eca6d3ae33dbfc45d1df4076bc22
thermojam <nmensky@gmail.com>
2025-12-29 18:24:24 +0300

    add docker

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -7,16 +8,22 @@ const routes = require('./routes');
 
 dotenv.config();
 
+const PORT = 3001;
 const app = express();
-
-const PORT = process.env.PORT || 3001;
 const MONGO_URL = process.env.MONGO_URL;
+
+const distPath = path.join(__dirname, '..', 'frontend', 'dist');
+app.use(express.static(distPath));
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 
 app.use('/api', routes);
+
+app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'));
+});
 
 mongoose.connect(MONGO_URL)
     .then(() => {
